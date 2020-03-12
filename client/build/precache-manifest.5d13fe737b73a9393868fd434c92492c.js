44cc99ba5c8b1de4d7a0702ba13f1c974834da81
JaneBane <lienaqperez19@gmail.com>
2020-03-11 23:08:23 -0400

    finished hw, issues with deploying to heroku

---
+++ client/build/precache-manifest.5d13fe737b73a9393868fd434c92492c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4576ee5e3accc964354c",
+    "url": "/static/css/main.34b6d86e.chunk.css"
+  },
+  {
+    "revision": "4576ee5e3accc964354c",
+    "url": "/static/js/main.3b9b2c17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "993d2b9486146bdb7afc",
+    "url": "/static/css/2.6b4153dc.chunk.css"
+  },
+  {
+    "revision": "993d2b9486146bdb7afc",
+    "url": "/static/js/2.947a4c27.chunk.js"
+  },
+  {
+    "revision": "406590ff86f7b4644c109545736c8d15",
+    "url": "/index.html"
+  }
+];
